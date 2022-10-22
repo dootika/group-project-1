d5ea74b01ab68bc6435b188b766d0ef54ccb23b5
Vijay1344 <106678595+Vijay1344@users.noreply.github.com>
2022-10-22 15:28:19 +0530

    Update vis_vijay.R

--- vis_vijay.R
+++ vis_vijay.R
@@ -1,1 +1,8 @@
-
+load("Anime_ranking.Rdata")
+library(ggplot2)
+### Barplot of source of anime with type
+g <- ggplot(data, aes(Source))
+g + geom_bar(aes(fill=Type), width = 0.5) + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Histogram on Categorial Variable of Anime Data ", 
+       subtitle="Source across Type ") 
