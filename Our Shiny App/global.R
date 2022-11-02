644815c973bbf1f72d1df587c05cb0fc1e6b2d89
Shlok Mishra <37180838+shlokmishra@users.noreply.github.com>
2022-11-02 19:36:25 +0530

    Add files via upload

---
+++ Our Shiny App/global.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(tidyr)
+library(googleVis)
+library(DT)
+library(dashboardthemes)
+library(GGally)
+
+anime1 <- dat
