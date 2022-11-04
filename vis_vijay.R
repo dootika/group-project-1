9958dc9c26b8f5cc7529ca7c4134fda485370251
Vijay1344 <106678595+Vijay1344@users.noreply.github.com>
2022-11-04 22:31:19 +0530

    Update vis_vijay.R

--- vis_vijay.R
+++ vis_vijay.R
@@ -6,3 +6,11 @@ g + geom_bar(aes(fill=Type), width = 0.5) +
   theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
   labs(title="Histogram on Categorial Variable of Anime Data ", 
        subtitle="Source across Type ") 
+####Graph of members vs Source 
+load("data1.Rdata")
+ library(ggplot2)
+ dat$Members <- as.numeric(as.character(dat$Members))
+ r <- ggplot(dat, aes(x = Final_Source, y = Members))
+ r + geom_boxplot(aes(col = Type )) + labs(title = "Boxpot",
+                                  subtitle = "Members VS Source Across Type",
+                                          x = "Source", y = "Members") 
